dc192b4c2e06501135c36afcbb525bc9ceebad65
María Fernanda Hernández Rodríguez <31429277+FernandaHernandezRodriguez@users.noreply.github.com>
2017-11-08 12:43:18 -0600

    Programa con correcciòn

--- ProgramaUno.cc
+++ ProgramaUno.cc
@@ -1,15 +1,19 @@
+//Maria Fernanda Hernandez Rodriguez
+//Resultado:13
+//Fecha de nacimiento: 28 de Noviembre de 1995
+
 #include <iostream>
 using namespace std;
 
 int isPrime(int X){
-  int flag=0; 
- 
+  int flag=0;
+
   for(int i=2; i<= (X/2) ; i++){
     if(X%i==0){
       flag=1;
       break;
     }
-  }   
+  }
   return flag;
 
 }
@@ -22,29 +26,30 @@ int main (){
   cout << "Dame un número entero" << endl;
   cin >> p;
 
- while (S!=0){ 
-   cout << "ciclo S!=0, S " << S << ",p " << p << endl;
-   while(p<S){
-       cout << "ciclo p<S, S " < S << ",p " << p << endl;
+  while (S!=0){
+    cout << "ciclo S!=0, S " << S << ",p " << p << endl;
+    while(p<S){
+      cout << "ciclo p<S, S " << S << ",p " << p << endl;
 
-    while(isPrime(p)==1){
-      cout << "ciclo isPrime, S " << S << ",p " << p << endl;
-      cout << p << " no es primo  " << endl;
-      if((p%2)==0)
-	p=p+1;
+      while(isPrime(p)==1){
+        cout << "ciclo isPrime, S " << S << ",p " << p << endl;
+        cout << p << " no es primo  " << endl;
+        if((p%2)==0)
+          p=p+1;
+        p=p+2;
+        cout << "final ciclo isPrime, S " << S << ",p " << p << endl;
+      }
+
+      S=S-p;
       p=p+2;
-      cout << "final ciclo isPrime, S " << S << ",p " << p << endl;
-    }  
-    
-    S=S-p;
-    p=p+2
-    cout << "final ciclo p<S, S " << S << ",p " << p << endl;
-   }
+      cout << "final ciclo p<S, S " << S << ",p " << p << endl;
+    }
 
-  S=S-1;
-  cout < "final ciclo S!=0, S " << S << ",p " << p << endl;
+    S=S-1;
+    cout << "final ciclo S!=0, S " << S << ",p " << p << endl;
  }
-  
+
   cout << " el valor de P es " << p << endl;
   return 0;
 }
+
